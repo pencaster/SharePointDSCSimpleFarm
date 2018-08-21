13ff96a664db5ddfbf091a29dd0643b6a4268d8b
pencaster <Krum@Haesli.net>
2018-08-21 10:37:23 +0200

    comments only
    Code comments changes ONLY!

--- DSC_Pullserver_Config.ps1
+++ DSC_Pullserver_Config.ps1
@@ -1,12 +1,4 @@
 #***************************************************************************************
-# This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.  
-# THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED 
-# TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS for A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free right to use and modify 
-# the Sample Code and to reproduce and distribute the object code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or 
-# trademarks to market Your software product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in 
-# which the Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, 
-# including attorneys fees, that arise or result from the use or distribution of the Sample Code.
-#
 # This script generates PullServer configuration, starts DSC feature, installs modules (If server has internet access), creates self signed cert and export root Ca 
 # (if you are using self signed cert), deploys IIS and creates DSC website/Web service,creates firewall rule, then runs Start-DscConfiguration on localmachine
 #
@@ -15,7 +7,12 @@
 # 
 # Don't forget to: Set-ExecutionPolicy RemoteSigned
 #
-# Written by Chris Weaver (christwe@microsoft.com)
+#
+# Author: Krum Haesli
+# Created: 21.08.2016
+# Modified: 
+# Comment:
+#
 #
 #****************************************************************************************
 
@@ -119,7 +116,7 @@ configuration DSC_PullServer_Config
 
 	Import-DSCResource -ModuleName xPSDesiredStateConfiguration
     Import-DSCResource -ModuleName PSDesiredStateConfiguration
-		 
+		ï¿½
 	Node $NodeName 
     { 
 	    WindowsFeature DSCServiceFeature 
